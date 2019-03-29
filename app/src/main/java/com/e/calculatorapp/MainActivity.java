086c2abb9d87dd6c6ff8b1ef4d30bc8d9ecb7c1d
Softwarica College Lab <softwarica@softwarica.com>
2019-03-29 09:00:35 +0545

    button added and button linked

--- app/src/main/java/com/e/calculatorapp/MainActivity.java
+++ app/src/main/java/com/e/calculatorapp/MainActivity.java
@@ -2,13 +2,51 @@ package com.e.calculatorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btnPlus,btnMinus,btnMul,btnDivide,btnCalc,btnClear,btnDot,btnBack;
+    private TextView tvRes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btnOne);
+        btn2 = findViewById(R.id.btnTwo);
+        btn3 = findViewById(R.id.btnThree);
+        btn4 = findViewById(R.id.btnFour);
+        btn5 = findViewById(R.id.btnFive);
+        btn6 = findViewById(R.id.btnSix);
+        btn7 = findViewById(R.id.btnSeven);
+        btn8 = findViewById(R.id.btnEight);
+        btn9 = findViewById(R.id.btnNine);
+        btn0 = findViewById(R.id.btnZero);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnMinus = findViewById(R.id.btnMinus);
+        btnMul = findViewById(R.id.btnMultiply);
+        btnDivide = findViewById(R.id.btnDivide);
+        btnCalc = findViewById(R.id.btnEnter);
+        btnClear = findViewById(R.id.btnClear);
+        btnDot = findViewById(R.id.btnDot);
+        btnBack = findViewById(R.id.btnBack);
+        tvRes = findViewById(R.id.tvResult);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvRes.setText("1");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvRes.setText("2");
+            }
+        });
 
     }
 }
